32ca3a967c1b14394438bfd2139a73d302668804
Valery Avila <valejo0310@gmail.com>
2026-05-26 17:29:39 -0500

    Update httpClient to use hardcoded API URL and timeout

--- assets/js/services/httpClient.js
+++ assets/js/services/httpClient.js
@@ -9,17 +9,15 @@ import axios from 'axios';
  * URL API desde variables de entorno
  * import.meta.env es soportado por Vite
  */
-const API_URL = import.meta.env.VITE_API_URL;
-const CONTENT_TYPE = import.meta.env.VITE_CONTENT_TYPE;
-const TIME_OUT = import.meta.env.VITE_TIME_OUT;
+
 /**
  * Instancia global Axios
  */
 const httpClient = axios.create({
-    baseURL: API_URL,
-    timeout: TIME_OUT,
+    baseURL: "https://rickandmortyapi.com/api",
+    timeout: 5000,
     headers: {
-        'Content-Type': CONTENT_TYPE
+        'Content-Type': 'application/json.'
     }
 });
 
